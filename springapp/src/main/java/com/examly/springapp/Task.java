050c893fe8c172f1ba35745125ca10a0a3273ff8
Iamneo <techadmin@iamneo.ai>
2023-07-31 17:20:06 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/Task.java
+++ springapp/src/main/java/com/examly/springapp/Task.java
@@ -1,3 +1,9 @@
+package com.examly.springapp;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
 @Entity
 public class Task {
     @Id
@@ -6,36 +12,59 @@ public class Task {
     private LocalDate taskDate;
     private String taskName;
     private String taskStatus;
-    public Long getTaskId() {
-        return taskId;
+
+    // Constructors, Getters, and Setters
+
+    public Task() {
     }
-    public void setTaskId(Long taskId) {
+
+    public Task(Long taskId, String taskHolderName, LocalDate taskDate, String taskName, String taskStatus) {
         this.taskId = taskId;
-    }
-    public String getTaskHolderName() {
-        return taskHolderName;
-    }
-    public void setTaskHolderName(String taskHolderName) {
         this.taskHolderName = taskHolderName;
-    }
-    public LocalDate getTaskDate() {
-        return taskDate;
-    }
-    public void setTaskDate(LocalDate taskDate) {
         this.taskDate = taskDate;
-    }
-    public String getTaskName() {
-        return taskName;
-    }
-    public void setTaskName(String taskName) {
         this.taskName = taskName;
-    }
-    public String getTaskStatus() {
-        return taskStatus;
-    }
-    public void setTaskStatus(String taskStatus) {
         this.taskStatus = taskStatus;
     }
 
-    // constructors, getters, and setters
+    // Getters and Setters
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskHolderName() {
+        return taskHolderName;
+    }
+
+    public void setTaskHolderName(String taskHolderName) {
+        this.taskHolderName = taskHolderName;
+    }
+
+    public LocalDate getTaskDate() {
+        return taskDate;
+    }
+
+    public void setTaskDate(LocalDate taskDate) {
+        this.taskDate = taskDate;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
 }
