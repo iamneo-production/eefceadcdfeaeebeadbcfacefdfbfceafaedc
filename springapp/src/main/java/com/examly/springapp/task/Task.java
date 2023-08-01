f2bb8d205d0ab9609898d944c17ca8e1c59bf123
Iamneo <techadmin@iamneo.ai>
2023-08-01 07:10:48 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/task/Task.java
+++ springapp/src/main/java/com/examly/springapp/task/Task.java
@@ -1,5 +1,71 @@
 package com.examly.springapp.task;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
 public class Task {
-    
+    @Id
+    private Long taskId;
+    private String taskHolderName;
+    private LocalDate taskDate;
+    private String taskName;
+    private String taskStatus;
+
+    // Constructors, Getters, and Setters
+
+    // Constructors
+    public Task() {
+    }
+
+    public Task(Long taskId, String taskHolderName, LocalDate taskDate, String taskName, String taskStatus) {
+        this.taskId = taskId;
+        this.taskHolderName = taskHolderName;
+        this.taskDate = taskDate;
+        this.taskName = taskName;
+        this.taskStatus = taskStatus;
+    }
+
+    // Getters and Setters
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskHolderName() {
+        return taskHolderName;
+    }
+
+    public void setTaskHolderName(String taskHolderName) {
+        this.taskHolderName = taskHolderName;
+    }
+
+    public LocalDate getTaskDate() {
+        return taskDate;
+    }
+
+    public void setTaskDate(LocalDate taskDate) {
+        this.taskDate = taskDate;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
 }
+
---
+++ springapp/src/main/java/com/examly/springapp/task/TaskController.java
@@ -0,0 +1,48 @@
+package com.examly.springapp.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TaskController {
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @PostMapping("/saveTask")
+    public Task saveTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @GetMapping("/changeStatus")
+    public Task changeTaskStatus(@RequestParam Long id, @RequestParam String status) {
+        Task task = taskRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Task not found with id: " + id));
+        task.setTaskStatus(status);
+        return taskRepository.save(task);
+    }
+
+    @GetMapping("/deleteTask")
+    public void deleteTask(@RequestParam Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    @GetMapping("/alltasks")
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @GetMapping("/getTask")
+    public Task getTaskById(@RequestParam Long id) {
+        return taskRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Task not found with id: " + id));
+    }
+
+    @GetMapping("/getTaskByHolderName")
+    public List<Task> getTaskByHolderName(@RequestParam String holderName) {
+        return taskRepository.findByTaskHolderName(holderName);
+    }
+}
